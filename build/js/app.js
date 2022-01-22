3c458aced2cfcce3b4836923d4eae2584111dac4
Alexander <sashakrvv@yandex.ru>
2022-01-22 22:08:29 +0400

    start creat footer

--- build/js/app.js
+++ build/js/app.js
@@ -1,3 +1,44 @@
 $("#exampleModal").on('hidden.bs.modal', function (e) {
 	$("#exampleModal iframe").attr("src", $("#exampleModal iframe").attr("src"));
 });
+
+$(function () {
+	// Header_fixed
+	let header = $("#header");
+	let intro = $("#intro");
+	let introH = intro.innerHeight();
+	let scrollPos = $(window).scrollTop();
+	let nav = $("#nav");
+	let navToggle = $("#navToggle");
+	checkScroll(scrollPos, introH);
+
+
+	$(window).on("scroll resize", function () {
+		let introH = intro.innerHeight();
+		let scrollPos = $(this).scrollTop();
+
+		checkScroll(scrollPos, introH);
+	});
+
+	function checkScroll(scrollPos, introH) {
+		if (scrollPos > introH - 20) {
+			header.addClass("fixed");
+		} else {
+			header.removeClass("fixed");
+		}
+	}
+
+	$("[data-scroll]").on("click", function (event) {
+		event.preventDefault();
+
+		let elementId = $(this).data('scroll');
+		let elementOffset = $(elementId).offset().top;
+		console.log(elementOffset);
+
+		nav.removeClass("show");
+
+		$("html, body").animate({
+			scrollTop: elementOffset - 50
+		}, 700);
+	});
+});
