089a927b6f2e35b4598e642365a3c568981e51f0
Alexander <sashakrvv@yandex.ru>
2022-01-23 23:27:39 +0400

    adaptive 100%

--- build/js/app.js
+++ build/js/app.js
@@ -33,7 +33,6 @@ $(function () {
 
 		let elementId = $(this).data('scroll');
 		let elementOffset = $(elementId).offset().top;
-		console.log(elementOffset);
 
 		nav.removeClass("show");
 
@@ -41,4 +40,69 @@ $(function () {
 			scrollTop: elementOffset - 50
 		}, 700);
 	});
+
+	// navToggle
+
+
+	navToggle.on("click", function (event) {
+		event.preventDefault();
+
+		nav.toggleClass("show");
+
+	})
+
 });
+
+//-----reviews ative---------
+
+function delClassActive() {
+	let div = document.querySelectorAll('.reviews__foto');
+	for (let i = 0; i < div.length; i++) {
+		for (let z = 0; z < div[i].classList.length; z++) {
+			if (div[i].classList[z] == 'focus-foto') {
+				div[i].classList.remove("focus-foto");
+			}
+		}
+	}
+
+	div = document.querySelectorAll('.reviews__item');
+	for (let i = 0; i < div.length; i++) {
+		for (let z = 0; z < div[i].classList.length; z++) {
+			if (div[i].classList[z] == 'focus-item') {
+				div[i].classList.remove("focus-item");
+			}
+		}
+	}
+
+}
+
+
+
+
+
+document.querySelector('.f1').onclick = function () {
+	delClassActive();
+	document.querySelector('.f1').classList.add('focus-foto');
+	document.querySelector('.i1').classList.add('focus-item');
+
+}
+document.querySelector('.f2').onclick = function () {
+	delClassActive();
+	document.querySelector('.f2').classList.add('focus-foto');
+	document.querySelector('.i2').classList.add('focus-item');
+}
+document.querySelector('.f3').onclick = function () {
+	delClassActive();
+	document.querySelector('.f3').classList.add('focus-foto');
+	document.querySelector('.i3').classList.add('focus-item');
+}
+document.querySelector('.f4').onclick = function () {
+	delClassActive();
+	document.querySelector('.f4').classList.add('focus-foto');
+	document.querySelector('.i4').classList.add('focus-item');
+}
+document.querySelector('.f5').onclick = function () {
+	delClassActive();
+	document.querySelector('.f5').classList.add('focus-foto');
+	document.querySelector('.i5').classList.add('focus-item');
+}
