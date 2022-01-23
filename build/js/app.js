83ddbaf811612333cc272495942647847b1ad7ec
Alexander <sashakrvv@yandex.ru>
2022-01-24 02:16:24 +0400

    done

--- build/js/app.js
+++ build/js/app.js
@@ -1,6 +1,5 @@
-$("#exampleModal").on('hidden.bs.modal', function (e) {
-	$("#exampleModal iframe").attr("src", $("#exampleModal iframe").attr("src"));
-});
+
+
 
 $(function () {
 	// Header_fixed
@@ -74,7 +73,7 @@ function delClassActive() {
 		}
 	}
 
-}
+};
 
 
 
@@ -85,24 +84,31 @@ document.querySelector('.f1').onclick = function () {
 	document.querySelector('.f1').classList.add('focus-foto');
 	document.querySelector('.i1').classList.add('focus-item');
 
-}
+};
 document.querySelector('.f2').onclick = function () {
 	delClassActive();
 	document.querySelector('.f2').classList.add('focus-foto');
 	document.querySelector('.i2').classList.add('focus-item');
-}
+};
 document.querySelector('.f3').onclick = function () {
 	delClassActive();
 	document.querySelector('.f3').classList.add('focus-foto');
 	document.querySelector('.i3').classList.add('focus-item');
-}
+};
 document.querySelector('.f4').onclick = function () {
 	delClassActive();
 	document.querySelector('.f4').classList.add('focus-foto');
 	document.querySelector('.i4').classList.add('focus-item');
-}
+};
 document.querySelector('.f5').onclick = function () {
 	delClassActive();
 	document.querySelector('.f5').classList.add('focus-foto');
 	document.querySelector('.i5').classList.add('focus-item');
-}
+};
+
+
+$(document).on('click', '#popup__close', function () {
+	jQuery("iframe").each(function () {
+		jQuery(this)[0].contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*')
+	});
+});
